08cec65eb2cda7359a9e207245dedaa9cbb1414a
zhuwq585 <zhuwq585@hotmail.com>
2018-03-24 20:20:55 +0800

    initial

---
+++ front/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header-bar></header-bar>
+    <stu-nav v-if="isStu"></stu-nav>
+    <mgr-nav v-if="!isStu"></mgr-nav>
+  </div>
+</template>
+
+<script>
+import mgrNav from '@/components/mgr/mgrNav'
+import stuNav from '@/components/stu/stuNav'
+import Header from '@/components/Header'
+export default {
+  name: 'App',
+  data: function(){
+    return{
+      isStu: false
+    }
+  },
+  components: {
+     'stu-nav' : stuNav,
+     'mgrNav' : mgrNav,
+     'header-bar': Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
